e4dd0ba9cbb6f0737e98d7c0f339231e1aea9cd1
Tim Daly <root@localhost.localdomain>
2007-09-25 22:12:34 -0400

    20071001 tpd src/algebra/exposed.lisp add (|AxiomServer| . AXSERV) to basic 20071001 tpd src/algebra/Makefile add axserver.spad 20071001 acr src/algebra/axserver.spad axserver socket connection code 20071001 tpd src/interp/Makefile add http.lisp 20071001 acr src/interp/http.lisp axserver socket connection code 20071001 acr license/license.ralfs added 20071001 acr Arthur C. Ralfs <arthur@mathbrane.ca>

---
+++ src/interp/http.lisp
@@ -0,0 +1,23 @@
+(in-package "BOOT")
+
+(defvar |StandardOutput| *standard-output*)
+
+(defvar  |NewLine| '#\NewLine)
+
+;; some regexp stuff
+
+(defun |StringMatch| (s1 s2) (si::string-match s1 s2))
+(defun |ListMatches| (&rest args) (si::list-matches args))
+(defun |MatchBeginning| (i) (si::match-beginning i))
+(defun |MatchEnd| (i) (si::match-end i))
+
+;; the socket stuff
+
+(defun |SiSock| (p spadfn)
+  (si::socket p :server
+   (function (lambda (w) (SPADCALL w spadfn) )) :daemon nil))
+
+(defun |SiListen| (s) (si::listen s))
+(defun |SiAccept| (s) (si::accept s))
+(defun |SiCopyStream| (q s) (si::copy-stream q s))
+
