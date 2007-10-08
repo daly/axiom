81d811361426c9b0f5409b860c35de04cb27c827
Tim Daly <root@localhost.localdomain>
2007-10-08 05:43:24 -0400

    20071019 acr src/interp/http.lisp use new return values 20071019 acr src/algebra/axserver.spad use new return values

--- src/interp/http.lisp
+++ src/interp/http.lisp
@@ -4,23 +4,45 @@
   (si::open path :direction :input :if-exists nil :if-does-not-exist nil)
   )
 
+(defvar |StandardOutput| *standard-output*)
 
 (defvar  |NewLine| '#\NewLine)
 
 ;; some regexp stuff
 
-(defun |StringMatch| (s1 s2) (si::string-match s1 s2))
-(defun |ListMatches| (&rest args) (si::list-matches args))
-(defun |MatchBeginning| (i) (si::match-beginning i))
-(defun |MatchEnd| (i) (si::match-end i))
+(defun |StringMatch| (s1 s2)
+ (si::string-match s1 s2)
+ )
+
+(defun |ListMatches| (&rest args)
+ (si::list-matches args)
+ )
+
+(defun |MatchBeginning| (i)
+ (si::match-beginning i)
+ )
+
+(defun |MatchEnd| (i)
+ (si::match-end i)
+ )
 
 ;; the socket stuff
 
 (defun |SiSock| (p spadfn)
-  (si::socket p :server
-   (function (lambda (w) (SPADCALL w spadfn) )) :daemon nil))
+;;  (format t "SiSocket-1")
+ (si::socket p :server
+             (function
+              (lambda (w) (SPADCALL w spadfn) )
+              )
+             :daemon nil)
+ )
 
-(defun |SiListen| (s) (si::listen s))
+(defun |SiListen| (s)
+;;  (format t "SiListen-1")
+ (si::listen s)
+ )
 (defun |SiAccept| (s) (si::accept s))
 (defun |SiCopyStream| (q s) (si::copy-stream q s))
+
+
 
